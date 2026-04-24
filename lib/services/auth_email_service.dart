fef15a10a99f4c12685e52700c841ace11960a4c
Anthony Puopolo <anpuop1511@gmail.com>
2026-04-24 12:03:54 -0400

    Use canonical auth email API domain

--- lib/services/auth_email_service.dart
+++ lib/services/auth_email_service.dart
@@ -7,7 +7,7 @@ class AuthEmailService {
 
   static const String _backendBaseUrl = String.fromEnvironment(
     'AUTH_EMAIL_API_BASE_URL',
-    defaultValue: 'https://homework-helper-web-dun.vercel.app/api',
+    defaultValue: 'https://www.hwhelper.tech/api',
   );
 
   static Uri get _sendAuthEmailUri =>
